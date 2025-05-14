96a40d3273596e2bf4e612d8eef97f9ae4f39c99
Faizalilham <faizalfalakh19@gmail.com>
2025-05-14 19:53:30 +0700

    Add fastlane to deploy playstore

---
+++ app/deploy_staging.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+export STAGING_PACKAGE_NAME="com.faizal.newsapp"
+
+export STAGING_KEYSTORE_FILE=":C:\Users\ACER\AndroidStudioProjects\NewsApp\jks\jksfile.jks"
+export STAGING_KEYSTORE_PASSWORD="Pendo1107"
+export STAGING_KEY_ALIAS="Pendo1107"
+export STAGING_KEY_PASSWORD="Pendo1107"
+
+export GPLAY_SERVICE_ACCOUNT_KEY="C:\Users\ACER\AndroidStudioProjects\NewsApp\app\service-json.json "
+
+fastlane deploy
